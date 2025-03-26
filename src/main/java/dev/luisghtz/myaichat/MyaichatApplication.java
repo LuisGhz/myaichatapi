682af20961c31be5d3b41696a6b93ba0c6229d56
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-03-26 00:47:22 +0000

    Init project

---
+++ src/main/java/dev/luisghtz/myaichat/MyaichatApplication.java
@@ -0,0 +1,13 @@
+package dev.luisghtz.myaichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaichatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/luisghtz/myaichat/MyaichatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luisghtz.myaichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaichatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
