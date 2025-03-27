a4556f228a80b73380cac3140c8444138753bb10
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-03-27 23:09:10 +0000

    Get messages

--- src/main/java/dev/luisghtz/myaichat/chat/ChatController.java
+++ src/main/java/dev/luisghtz/myaichat/chat/ChatController.java
@@ -7,16 +7,19 @@ import org.springframework.web.server.ResponseStatusException;
 import dev.luisghtz.myaichat.chat.dtos.AssistantMessageResponseDto;
 import dev.luisghtz.myaichat.chat.dtos.ChatErrorResponseDto;
 import dev.luisghtz.myaichat.chat.dtos.ChatsListResponseDto;
+import dev.luisghtz.myaichat.chat.dtos.HistoryChatDto;
 import dev.luisghtz.myaichat.chat.dtos.NewMessageRequestDto;
 import dev.luisghtz.myaichat.chat.services.AIService;
-import io.micrometer.core.ipc.http.HttpSender.Response;
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("api/chat")
@@ -24,10 +27,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ChatController {
   private final AIService aiService;
 
-  @GetMapping("chats")
+  @GetMapping("all")
   public ResponseEntity<ChatsListResponseDto> getChatsList() {
     var chats = aiService.getAllChats();
     return ResponseEntity.ok(chats);
+  }
+
+  @GetMapping("{id}/messages")
+  public ResponseEntity<HistoryChatDto> getChatHistory(@PathVariable UUID id) {
+    return ResponseEntity.ok(aiService.getChatHistory(id));
   }
 
   @PostMapping("send-message")
