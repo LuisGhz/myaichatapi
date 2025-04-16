e901a338f6ced9e1993dad2ee672d2ce6d3de2cc
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-16 15:46:50 -0600

    Register prompt with their messages and params

--- src/main/java/dev/luisghtz/myaichat/prompts/services/CustomPromptService.java
+++ src/main/java/dev/luisghtz/myaichat/prompts/services/CustomPromptService.java
@@ -1,10 +1,15 @@
 package dev.luisghtz.myaichat.prompts.services;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.luisghtz.myaichat.prompts.dtos.CreateCustomPromptDtoReq;
 import dev.luisghtz.myaichat.prompts.dtos.PromptsListDtoRes;
 import dev.luisghtz.myaichat.prompts.entities.CustomPrompt;
+import dev.luisghtz.myaichat.prompts.entities.PromptMessage;
+import dev.luisghtz.myaichat.prompts.entities.PromptParam;
 import dev.luisghtz.myaichat.prompts.repositories.CustomRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -19,12 +24,44 @@ public class CustomPromptService {
     return promptsDto;
   }
 
+  @Transactional
   public CustomPrompt create(CreateCustomPromptDtoReq createCustomPromptDtoReq) {
     var newCustomPrompt = CustomPrompt.builder()
         .name(createCustomPromptDtoReq.getName())
-        .model(createCustomPromptDtoReq.getModel())
-        .systemMessage(createCustomPromptDtoReq.getSystemMessage())
+        .content(createCustomPromptDtoReq.getContent())
+        .createdAt(new Date())
         .build();
+    addPromptMessagesIfExists(createCustomPromptDtoReq, newCustomPrompt);
+    addPromptParamsIfExists(createCustomPromptDtoReq, newCustomPrompt);
     return promptRepository.save(newCustomPrompt);
+  }
+
+  private void addPromptMessagesIfExists(CreateCustomPromptDtoReq createCustomPromptDtoReq,
+      CustomPrompt customPrompt) {
+    if (createCustomPromptDtoReq.getMessages() != null && !createCustomPromptDtoReq.getMessages().isEmpty()) {
+      var messages = createCustomPromptDtoReq.getMessages().stream().map(msg -> {
+        return PromptMessage.builder()
+            .role(msg.getRole())
+            .content(msg.getContent())
+            .prompt(customPrompt)
+            .build();
+      }).toList();
+
+      customPrompt.setMessages(messages);
+    }
+  }
+
+  private void addPromptParamsIfExists(CreateCustomPromptDtoReq createCustomPromptDtoReq, CustomPrompt customPrompt) {
+    if (createCustomPromptDtoReq.getParams() != null && !createCustomPromptDtoReq.getParams().isEmpty()) {
+      var params = createCustomPromptDtoReq.getParams().stream().map(param -> {
+        return PromptParam.builder()
+            .name(param.getName())
+            .value(param.getValue())
+            .prompt(customPrompt)
+            .build();
+      }).toList();
+
+      customPrompt.setParams(params);
+    }
   }
 }
