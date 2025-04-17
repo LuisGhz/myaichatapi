ec5749cd993d7127ec352e98466c4d815330daca
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-17 17:24:06 -0600

    Add exceptions AppMethodArgumentNotValidException AppNotFoundException

--- src/main/java/dev/luisghtz/myaichat/advices/GlobalControllerAdvice.java
+++ src/main/java/dev/luisghtz/myaichat/advices/GlobalControllerAdvice.java
@@ -2,6 +2,7 @@ package dev.luisghtz.myaichat.advices;
 
 import java.util.stream.Collectors;
 
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,15 +10,29 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import dev.luisghtz.myaichat.chat.dtos.ChatErrorResponseDto;
+import dev.luisghtz.myaichat.exceptions.AppMethodArgumentNotValidException;
+import dev.luisghtz.myaichat.exceptions.AppNotFoundException;
 import dev.luisghtz.myaichat.exceptions.ImageNotValidException;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
-  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ExceptionHandler({ MethodArgumentNotValidException.class })
   public ResponseEntity<ChatErrorResponseDto> handleValidationException(MethodArgumentNotValidException ex) {
     String message = ex.getBindingResult().getFieldErrors().stream()
         .map(error -> error.getField() + ": " + error.getDefaultMessage())
         .collect(Collectors.joining(", "));
+
+    var response = ChatErrorResponseDto.builder()
+        .statusCode(HttpStatus.BAD_REQUEST)
+        .message("Validation error: " + message)
+        .build();
+
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler({ AppMethodArgumentNotValidException.class })
+  public ResponseEntity<ChatErrorResponseDto> handleValidationException(AppMethodArgumentNotValidException ex) {
+    String message = ex.getMessage();
 
     var response = ChatErrorResponseDto.builder()
         .statusCode(HttpStatus.BAD_REQUEST)
@@ -35,6 +50,16 @@ public class GlobalControllerAdvice {
         .build();
 
     return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(AppNotFoundException.class)
+  public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
+    var response = ChatErrorResponseDto.builder()
+        .statusCode(HttpStatus.NOT_FOUND)
+        .message(ex.getMessage())
+        .build();
+
+    return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(RuntimeException.class)
