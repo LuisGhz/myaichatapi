d57957e77fac5e15d068a69fd359e161cfd12c01
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-03 18:13:18 -0600

    Create beans for openai and vertex chat models

--- src/main/java/dev/luisghtz/myaichat/MyaichatApplication.java
+++ src/main/java/dev/luisghtz/myaichat/MyaichatApplication.java
@@ -1,7 +1,11 @@
 package dev.luisghtz.myaichat;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyaichatApplication {
@@ -9,5 +13,15 @@ public class MyaichatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyaichatApplication.class, args);
 	}
+
+  @Bean
+  ChatClient openAIChatClient(OpenAiChatModel openAiChatModel) {
+    return ChatClient.create(openAiChatModel);
+  }
+
+  @Bean
+  ChatClient vertextAIChatClient(VertexAiGeminiChatModel vertexAiChatModel) {
+    return ChatClient.create(vertexAiChatModel);
+  }
 
 }
