a9b6bb7c8e814af9afdf0f68400920c63aa834ad
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-03 18:57:51 -0600

    Add @AllowedStringValues to NewMessageRequestDto

--- src/main/java/dev/luisghtz/myaichat/chat/dtos/NewMessageRequestDto.java
+++ src/main/java/dev/luisghtz/myaichat/chat/dtos/NewMessageRequestDto.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.web.multipart.MultipartFile;
 
 import dev.luisghtz.myaichat.image.validators.ValidImage;
+import dev.luisghtz.myaichat.validators.AllowedStringValues;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class NewMessageRequestDto {
   private UUID chatId = null;
   @NotEmpty
   private String prompt;
+  @AllowedStringValues(values = { "gpt-4o", "gpt-4o-mini", "gemini-pro", "gemini-2.0-flash" })
   private String model = null;
   @ValidImage(message = "Invalid image: must be a valid image file.")
   private MultipartFile image = null;
