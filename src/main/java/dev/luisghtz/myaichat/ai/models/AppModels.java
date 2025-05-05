f03e0f49e495a0ec6d80deb3a1a4b11e5145e4e1
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-05 16:27:55 -0600

    Update versions 2.5

--- src/main/java/dev/luisghtz/myaichat/ai/models/AppModels.java
+++ src/main/java/dev/luisghtz/myaichat/ai/models/AppModels.java
@@ -8,8 +8,8 @@ public class AppModels {
   public static final AppModel GPT_4O = new AppModel("gpt-4o", 5000);
   public static final AppModel GEMINI_FLASH_2_0_LITE = new AppModel("gemini-2.0-flash-lite", 2500);
   public static final AppModel GEMINI_FLASH_2_0 = new AppModel("gemini-2.0-flash", 5000);
-  public static final AppModel GEMINI_FLASH_2_5_FLASH = new AppModel("gemini-2.5-flash", 5000);
-  public static final AppModel GEMINI_FLASH_2_5_PRO = new AppModel("gemini-2.5-pro", 5000);
+  public static final AppModel GEMINI_FLASH_2_5_FLASH = new AppModel("gemini-2.5-flash-preview-04-17", 5000);
+  public static final AppModel GEMINI_FLASH_2_5_PRO = new AppModel("gemini-2.5-pro-preview-03-25", 5000);
 
   private static final Map<String, Integer> MODEL_MAX_TOKENS = Map.of(
       GPT_4O_MINI.getKey(), GPT_4O_MINI.getMaxTokens(),
