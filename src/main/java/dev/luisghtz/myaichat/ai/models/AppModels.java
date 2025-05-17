45a921d4b3a241b722eb5257f8b8b298e0ab005b
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-17 15:11:27 -0600

    Update max output tokens.

--- src/main/java/dev/luisghtz/myaichat/ai/models/AppModels.java
+++ src/main/java/dev/luisghtz/myaichat/ai/models/AppModels.java
@@ -4,12 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 public class AppModels {
-  public static final AppModel GPT_4O_MINI = new AppModel("gpt-4o-mini", 2500);
-  public static final AppModel GPT_4O = new AppModel("gpt-4o", 5000);
-  public static final AppModel GEMINI_FLASH_2_0_LITE = new AppModel("gemini-2.0-flash-lite", 2500);
-  public static final AppModel GEMINI_FLASH_2_0 = new AppModel("gemini-2.0-flash", 5000);
-  public static final AppModel GEMINI_FLASH_2_5_FLASH = new AppModel("gemini-2.5-flash-preview-04-17", 5000);
-  public static final AppModel GEMINI_FLASH_2_5_PRO = new AppModel("gemini-2.5-pro-preview-03-25", 5000);
+  public static final AppModel GPT_4O_MINI = new AppModel("gpt-4o-mini", 5_000);
+  public static final AppModel GPT_4O = new AppModel("gpt-4o", 10_000);
+  public static final AppModel GEMINI_FLASH_2_0_LITE = new AppModel("gemini-2.0-flash-lite", 5_000);
+  public static final AppModel GEMINI_FLASH_2_0 = new AppModel("gemini-2.0-flash", 8_000);
+  public static final AppModel GEMINI_FLASH_2_5_FLASH = new AppModel("gemini-2.5-flash-preview-04-17", 10_000);
+  public static final AppModel GEMINI_FLASH_2_5_PRO = new AppModel("gemini-2.5-pro-preview-03-25", 10_000);
 
   private static final Map<String, Integer> MODEL_MAX_TOKENS = Map.of(
       GPT_4O_MINI.getKey(), GPT_4O_MINI.getMaxTokens(),
