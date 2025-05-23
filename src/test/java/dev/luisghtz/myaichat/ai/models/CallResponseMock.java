3dd61b9a74de7c639692a23a46f25e64e5dca323
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-23 12:50:20 -0600

    Create CallResponseMock

---
+++ src/test/java/dev/luisghtz/myaichat/ai/models/CallResponseMock.java
@@ -0,0 +1,59 @@
+package dev.luisghtz.myaichat.ai.models;
+
+import org.springframework.ai.chat.client.ChatClient.CallResponseSpec;
+import org.springframework.ai.chat.client.ResponseEntity;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.converter.StructuredOutputConverter;
+import org.springframework.core.ParameterizedTypeReference;
+
+import com.google.auto.value.AutoValue.Builder;
+
+import lombok.AllArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+public class CallResponseMock implements CallResponseSpec {
+
+  private ChatResponse chatResponse;
+
+  @Override
+  public <T> T entity(ParameterizedTypeReference<T> type) {
+    return null;
+  }
+
+  @Override
+  public <T> T entity(StructuredOutputConverter<T> structuredOutputConverter) {
+    return null;
+  }
+
+  @Override
+  public <T> T entity(Class<T> type) {
+    return null;
+  }
+
+  @Override
+  public ChatResponse chatResponse() {
+    return chatResponse;
+  }
+
+  @Override
+  public String content() {
+    return null;
+  }
+
+  @Override
+  public <T> ResponseEntity<ChatResponse, T> responseEntity(Class<T> type) {
+    return null;
+  }
+
+  @Override
+  public <T> ResponseEntity<ChatResponse, T> responseEntity(ParameterizedTypeReference<T> type) {
+    return null;
+  }
+
+  @Override
+  public <T> ResponseEntity<ChatResponse, T> responseEntity(StructuredOutputConverter<T> structuredOutputConverter) {
+    return null;
+  }
+  
+}
