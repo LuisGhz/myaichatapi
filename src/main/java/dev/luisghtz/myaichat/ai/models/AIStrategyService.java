a0ef3964e5266d4a6ff93c9b088a8b105a9856ab
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-23 12:47:54 -0600

    Create AIStrategyService

---
+++ src/main/java/dev/luisghtz/myaichat/ai/models/AIStrategyService.java
@@ -0,0 +1,14 @@
+package dev.luisghtz.myaichat.ai.models;
+
+import java.util.List;
+
+import org.springframework.ai.chat.model.ChatResponse;
+
+import dev.luisghtz.myaichat.chat.entities.AppMessage;
+import dev.luisghtz.myaichat.chat.entities.Chat;
+
+public interface AIStrategyService {
+  ChatResponse sendNewMessage(List<AppMessage> messages, Chat chat);
+
+  String generateTitle(Chat chat, String userMessage, String assistantMessage);
+}
