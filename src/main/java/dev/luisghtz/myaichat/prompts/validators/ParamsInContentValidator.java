87f7707025f311783f9d0e5096356ff9dd3e2d99
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-25 14:21:13 -0600

    Create ParamsInContent validator

---
+++ src/main/java/dev/luisghtz/myaichat/prompts/validators/ParamsInContentValidator.java
@@ -0,0 +1,35 @@
+package dev.luisghtz.myaichat.prompts.validators;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import dev.luisghtz.myaichat.prompts.dtos.CreateCustomPromptDtoReq;
+import dev.luisghtz.myaichat.prompts.dtos.CreateCustomPromptParamsDto;
+
+public class ParamsInContentValidator implements ConstraintValidator<ParamsInContent, CreateCustomPromptDtoReq> {
+  private String message;
+
+  @Override
+  public void initialize(ParamsInContent constraintAnnotation) {
+    this.message = constraintAnnotation.message();
+  }
+
+  @Override
+  public boolean isValid(CreateCustomPromptDtoReq value, ConstraintValidatorContext context) {
+    if (value.getParams() == null || value.getParams().isEmpty())
+      return true;
+    for (CreateCustomPromptParamsDto param : value.getParams()) {
+      if (!value.getContent().contains("{" + param.getName() + "}")) {
+        context.disableDefaultConstraintViolation();
+
+        // Add custom error message
+        context.buildConstraintViolationWithTemplate(
+            message)
+            .addConstraintViolation();
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+}
