22e6340e8e1cd59ac28a62b0d622065924f93d17
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-26 12:09:51 -0600

    Add validation for specific images types

--- src/main/java/dev/luisghtz/myaichat/image/validators/ImageValidator.java
+++ src/main/java/dev/luisghtz/myaichat/image/validators/ImageValidator.java
@@ -1,5 +1,7 @@
 package dev.luisghtz.myaichat.image.validators;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,6 +11,7 @@ import jakarta.validation.ConstraintValidatorContext;
 public class ImageValidator implements ConstraintValidator<ValidImage, MultipartFile> {
   @Value("${files.max-mb-size}")
   private short maxMBSize;
+  private List<String> imageTypes = List.of("image/jpg", "image/jpeg", "image/png", "image/gif");
 
   @Override
   public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
@@ -30,12 +33,12 @@ public class ImageValidator implements ConstraintValidator<ValidImage, Multipart
       return false;
     }
 
-    if (!contentType.startsWith("image/")) {
+    if (!imageTypes.contains(contentType)) {
       context.disableDefaultConstraintViolation();
       context.buildConstraintViolationWithTemplate("File is not an image").addConstraintViolation();
       return false;
     }
-    
+
     return true;
   }
 
