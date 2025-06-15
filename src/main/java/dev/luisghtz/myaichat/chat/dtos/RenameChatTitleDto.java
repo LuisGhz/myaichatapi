cbffffe08c6931e8069a347fc803bb3fda4b7a95
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-15 14:33:05 -0600

    Rename chat title DTO

---
+++ src/main/java/dev/luisghtz/myaichat/chat/dtos/RenameChatTitleDto.java
@@ -0,0 +1,13 @@
+package dev.luisghtz.myaichat.chat.dtos;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RenameChatTitleDto {
+  @NotBlank
+  @Length(min = 1, max = 50)
+  private String title;
+}
