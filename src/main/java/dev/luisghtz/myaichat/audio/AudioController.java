0d2099c6356840b0c4ed8dda1a563356445945ed
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-14 11:20:42 -0600

    Create audio controller with transcription service

---
+++ src/main/java/dev/luisghtz/myaichat/audio/AudioController.java
@@ -0,0 +1,27 @@
+package dev.luisghtz.myaichat.audio;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.luisghtz.myaichat.audio.dto.TranscriptionReqDto;
+import dev.luisghtz.myaichat.audio.dto.TranscriptionResDto;
+import dev.luisghtz.myaichat.audio.services.TranscribeService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/api/audio")
+@RequiredArgsConstructor
+public class AudioController {
+  private final TranscribeService transcribeService;
+
+  @PostMapping("transcribe")
+  public ResponseEntity<TranscriptionResDto> transcribeAudio(@ModelAttribute TranscriptionReqDto dto) {
+    TranscriptionResDto result = transcribeService.transcribe(dto.getAudio());
+    return ResponseEntity.ok(result);
+  }
+
+}
