03a514c1d3ef521c90bf7adcc41151caafd7ef73
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-15 16:46:04 -0600

    Add not null validation

--- src/main/java/dev/luisghtz/myaichat/audio/dto/TranscriptionReqDto.java
+++ src/main/java/dev/luisghtz/myaichat/audio/dto/TranscriptionReqDto.java
@@ -2,6 +2,7 @@ package dev.luisghtz.myaichat.audio.dto;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TranscriptionReqDto {
+  @NotNull(message = "Audio file is required")
   private MultipartFile audio;
 }
