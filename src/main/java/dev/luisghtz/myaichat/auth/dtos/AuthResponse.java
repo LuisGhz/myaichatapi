1d96206786acf8e3dd9b3dd3b5581585127404a1
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-19 13:36:52 -0600

    Create AuthResponse

---
+++ src/main/java/dev/luisghtz/myaichat/auth/dtos/AuthResponse.java
@@ -0,0 +1,19 @@
+package dev.luisghtz.myaichat.auth.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String type = "Bearer";
+    private UserDto user;
+    
+    public AuthResponse(String token, UserDto user) {
+        this.token = token;
+        this.user = user;
+    }
+}
