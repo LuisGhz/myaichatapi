96c2e7816f5f711d4f7ff90fa5a96833c4603205
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-20 12:27:48 -0600

    Apply format

--- src/main/java/dev/luisghtz/myaichat/auth/entities/User.java
+++ src/main/java/dev/luisghtz/myaichat/auth/entities/User.java
@@ -14,50 +14,50 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-    
-    @Column(nullable = false, unique = true)
-    private String username;
-    
-    @Column(nullable = false, unique = true)
-    private String email;
-    
-    @Column
-    private String password;
-    
-    @Column(nullable = false)
-    private Boolean locked = false;
-    
-    @Column(nullable = false)
-    private Boolean disabled = false;
-    
-    @OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "role_id")
-    private Role role;
-    
-    @Column(name = "github_id")
-    private String githubId;
-    
-    @Column(name = "avatar_url")
-    private String avatarUrl;
-    
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-    
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-    
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-        updatedAt = LocalDateTime.now();
-    }
-    
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column
+  private String password;
+
+  @Column(nullable = false)
+  private Boolean locked = false;
+
+  @Column(nullable = false)
+  private Boolean disabled = false;
+
+  @OneToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "role_id")
+  private Role role;
+
+  @Column(name = "github_id")
+  private String githubId;
+
+  @Column(name = "avatar_url")
+  private String avatarUrl;
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  protected void onCreate() {
+    createdAt = LocalDateTime.now();
+    updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    updatedAt = LocalDateTime.now();
+  }
 }
