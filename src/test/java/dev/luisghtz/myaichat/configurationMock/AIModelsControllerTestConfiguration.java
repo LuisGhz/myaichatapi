5c6746b48a24b979b7f5144d6ebcdd7aa1e58353
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-23 08:37:00 -0600

    Move the `JwtPermitAllTestConfiguration`

--- src/test/java/dev/luisghtz/myaichat/configurationMock/AIModelsControllerTestConfiguration.java
+++ src/test/java/dev/luisghtz/myaichat/configurationMock/AIModelsControllerTestConfiguration.java
@@ -11,63 +11,51 @@ import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
 import static org.mockito.Mockito.mock;
 
 @TestConfiguration
 public class AIModelsControllerTestConfiguration {
 
-    @Bean
-    @Primary
-    OpenAiChatModel openAiChatModel() {
-        return mock(OpenAiChatModel.class);
-    }
+  @Bean
+  @Primary
+  OpenAiChatModel openAiChatModel() {
+    return mock(OpenAiChatModel.class);
+  }
 
-    @Bean
-    @Primary
-    VertexAiGeminiChatModel vertexAiGeminiChatModel() {
-        return mock(VertexAiGeminiChatModel.class);
-    }
+  @Bean
+  @Primary
+  VertexAiGeminiChatModel vertexAiGeminiChatModel() {
+    return mock(VertexAiGeminiChatModel.class);
+  }
 
-    @Bean
-    @Primary
-    ChatClient openAIChatClient() {
-        return mock(ChatClient.class);
-    }
+  @Bean
+  @Primary
+  ChatClient openAIChatClient() {
+    return mock(ChatClient.class);
+  }
 
-    @Bean
-    @Primary 
-    ChatClient vertextAIChatClient() {
-        return mock(ChatClient.class);
-    }
+  @Bean
+  @Primary
+  ChatClient vertextAIChatClient() {
+    return mock(ChatClient.class);
+  }
 
-    @Bean
-    @Primary
-    Model<AudioTranscriptionPrompt, AudioTranscriptionResponse> audioTranscriptionModel() {
-        return mock(Model.class);
-    }
+  @Bean
+  @Primary
+  Model<AudioTranscriptionPrompt, AudioTranscriptionResponse> audioTranscriptionModel() {
+    return mock(Model.class);
+  }
 
-    @Bean
-    @Primary
-    JwtService jwtService() {
-        return mock(JwtService.class);
-    }
+  @Bean
+  @Primary
+  JwtService jwtService() {
+    return mock(JwtService.class);
+  }
 
-    @Bean
-    @Primary
-    UserService userService() {
-        return mock(UserService.class);
-    }
-
-    @Bean
-    @Primary
-    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authz -> authz.anyRequest().permitAll())
-                .build();
-    }
+  @Bean
+  @Primary
+  UserService userService() {
+    return mock(UserService.class);
+  }
 }
