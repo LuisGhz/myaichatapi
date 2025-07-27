b0ded7ca033bce97428335ca130d891629920bb8
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-27 15:12:48 -0600

    Add web search instructions

--- src/main/java/dev/luisghtz/myaichat/ai/utils/MessagesUtil.java
+++ src/main/java/dev/luisghtz/myaichat/ai/utils/MessagesUtil.java
@@ -1,5 +1,6 @@
 package dev.luisghtz.myaichat.ai.utils;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.ai.chat.messages.AssistantMessage;
@@ -14,11 +15,12 @@ public class MessagesUtil {
   public static void addSystemMessage(Chat chat, List<Message> messages) {
     if (chat.getCustomPrompt() != null) {
       CustomPrompt customPrompt = chat.getCustomPrompt();
-      messages.add(new SystemMessage(customPrompt.getContent()));
+      var prompt = addWebSearchInstructionsIfApply(chat, customPrompt.getContent());
+      messages.add(new SystemMessage(prompt));
       return;
     }
-
-    messages.add(new SystemMessage("You are a helpful assistant."));
+    var prompt = addWebSearchInstructionsIfApply(chat, "You are a helpful assistant.");
+    messages.add(new SystemMessage(prompt));
   }
 
   public static void addInitialMessagesIfApply(Chat chat, List<Message> messages) {
@@ -33,5 +35,21 @@ public class MessagesUtil {
         }
       });
     }
+  }
+
+  private static String addWebSearchInstructionsIfApply(Chat chat, String prompt) {
+    var currentDate = LocalDate.now();
+    var year = currentDate.getYear();
+    if (chat.getIsWebSearchMode()) {
+      var webSearchInstructions = String.format(
+          """
+              --- WEB SEARCH INSTRUCTIONS ---
+              When you use "web_search" you can search until %s so avoid to search with your cutoff date, for instance search "President of USA %d" instead of "President of USA 2023" where 2023 is the cutoff year for some models.
+              """,
+          currentDate, year);
+      return prompt += webSearchInstructions;
+    }
+
+    return prompt;
   }
 }
