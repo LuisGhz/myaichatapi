2cc8bf28563b8ebbff16c63e5fe23e211ba9fb4a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-28 10:20:19 -0600

    Change property type

--- src/main/java/dev/luisghtz/myaichat/chat/dtos/ChangeIsWebSearchModeReqDto.java
+++ src/main/java/dev/luisghtz/myaichat/chat/dtos/ChangeIsWebSearchModeReqDto.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChangeIsWebSearchModeReqDto {
   @NotNull
-  private boolean isWebSearchMode;
+  private Boolean isWebSearchMode;
 }
