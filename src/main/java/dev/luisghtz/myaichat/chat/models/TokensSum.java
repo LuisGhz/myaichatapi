7753b49d854050482dadea63dbf0aeae8b758068
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 10:18:53 -0600

    Add @ToString

--- src/main/java/dev/luisghtz/myaichat/chat/models/TokensSum.java
+++ src/main/java/dev/luisghtz/myaichat/chat/models/TokensSum.java
@@ -2,9 +2,11 @@ package dev.luisghtz.myaichat.chat.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
+@ToString
 public class TokensSum {
   private Long promptTokens;
   private Long completionTokens;
